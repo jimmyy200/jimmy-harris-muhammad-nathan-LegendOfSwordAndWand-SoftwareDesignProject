aae6ebe27ae93f6a386424b796a0ff3bcade2298
Harris Vidyavinodhan <harris06v@gmail.com>
2026-03-23 08:57:32 -0400

    Delete jimmy-nathan-muhammad-nathan-LegendOfSwordAndWand/src/main/java/test/java directory

--- jimmy-nathan-muhammad-nathan-LegendOfSwordAndWand/src/main/java/test/java/Observer/GameObserver.java
+++
@@ -1,9 +0,0 @@
-package Observer;
-
-// Observer pattern - observers get notified of game events
-public interface GameObserver {
-    void onHeroLevelUp(String heroName, int newLevel, String className);
-    void onHeroDefeated(String heroName);
-    void onGoldChanged(int newGold);
-    void onExperienceGained(String heroName, int xpGained);
-}
--- jimmy-nathan-muhammad-nathan-LegendOfSwordAndWand/src/main/java/test/java/Observer/Subject.java
+++
@@ -1,8 +0,0 @@
-package Observer;
-
-// Subject interface for Observer pattern - defines how observers register and get notified
-public interface Subject {
-    void attach(GameObserver observer);
-    void detach(GameObserver observer);
-    void notifyObservers();
-}
